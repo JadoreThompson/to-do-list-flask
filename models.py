9762c164843e860625d26f2dba5ce056c5bc5fc0
LucioJamieAI <47329590+LucioJamieAI@users.noreply.github.com>
2024-05-30 11:51:33 +0100

    All task now only shows completed

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 
+db = SQLAlchemy()
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(300), nullable=True)
-    done = db.Column(db.Boolean, default=False)
+    done = db.Column(db.Integer, default=0)
--- views.py
+++ views.py
@@ -3,18 +3,27 @@ from models import db, Task
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
-    tasks = Task.query.all()
+    tasks = Task.query.filter_by(done=0).all()
+    print("Tasks", tasks)
+
     return render_template('home.html', tasks=tasks)
+
 
 @views.route('/create-task', methods=['POST', 'GET'])
 def create_task():
     if request.method == "POST":
         def get_form_data():
-            title = request.form.get('name')
-            description = request.form.get('description')
-            completed = bool(request.form.get('completed'))
+            title = request.form['name']
+            description = request.form['description']
+            completed = request.form['completed']
+
+            print("Title", title)
+            print("Description", description)
+            print("Completed", completed)
+
             task = Task(title=title, description=description, done=completed)
             return task
 
@@ -22,9 +31,11 @@ def create_task():
         db.session.add(task)
         db.session.commit()
 
+
         return redirect(url_for('views.home'))
 
     return render_template('create-task.html')
+
 
 @views.route('/delete-task/<int:id>')
 def delete_task(id):
@@ -34,18 +45,28 @@ def delete_task(id):
 
     return redirect(url_for('views.home'))
 
+
 @views.route('/update-task/<int:id>', methods=['POST', 'GET'])
 def update_task(id):
-    task = Task.query.filter_by(id=id).first()
+    task = Task.query.get(id)
+    if task:
+        if request.method == "POST":
+            title = request.form.get('name')
+            description = request.form.get('description')
+            done = bool(request.form.get('completed'))
 
-    if request.method == "POST":
-        task.title = request.form.get('name')
-        task.description = request.form.get('description')
-        task.done = bool(request.form.get('completed'))
+            if title is not None:
+                task.title = title
+            if description is not None:
+                task.description = description
+            if done is not None:
+                task.done = done
 
+            task.verified = True
+            db.session.commit()
 
-        db.session.commit()
-
-        return redirect(url_for('views.home'))
+            return redirect(url_for('views.home'))
+    else:
+        return "Task not found!", 404
 
     return render_template('update-task.html', task=task)
